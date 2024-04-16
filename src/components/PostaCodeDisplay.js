ffbfef4f217451678dac40e3ea03bcb7a78f5364
Saulius Petrusevičius <petruseviciuss@gmail.com>
2024-04-17 00:40:54 +0300

    Postal Code display

--- src/components/PostaCodeDisplay.js
+++ src/components/PostaCodeDisplay.js
@@ -1,1 +1,14 @@
- 
+import React from "react";
+
+export function GeoDataDisplay({ data }) {
+  return (
+    <div>
+      <h2>Geo Data</h2>
+      <p>Postcode: {data.postcode}</p>
+      <p>Country: {data.country}</p>
+      <p>Longitude: {data.longitude}</p>
+      <p>Latitude: {data.latitude}</p>
+      <p>Admin District: {data.adminDistrict}</p>
+    </div>
+  );
+}
