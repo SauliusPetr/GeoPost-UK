9fbf2b35150d45bc71eb768c40f9c5732b49b8aa
Saulius Petrusevičius <petruseviciuss@gmail.com>
2024-04-17 00:45:44 +0300

    Starting History Component

--- src/components/PostalCodeHistory.js
+++ src/components/PostalCodeHistory.js
@@ -1,1 +1,21 @@
- 
+import React from "react";
+
+export function PostalCodeHistory({ savedPostalCode }) {
+  function displayFunction() {
+    //display this object
+  }
+  function deleteFunction() {
+    //delete this object
+  }
+  return (
+    <div>
+      {/* loop through ach item, display postalcode and delete option */}
+      {savedPostalCode.map((postalCodeObject) => {
+        <div>
+          <div onClick={displayFunction}>{postalCodeObject.postalcode}</div>
+          <div onClick={deleteFunction}>-</div>
+        </div>;
+      })}
+    </div>
+  );
+}
